37a56b623ad84e0cea7763a8d39f6adec0e22efd
KlayFouly <50269153+KlayFouly@users.noreply.github.com>
2025-03-20 09:30:22 +0100

    Add files via upload

---
+++ Installers/README.txt
@@ -0,0 +1,56 @@
+Les scripts sont encore en construction, et peuvent contenir des erreurs.
+
+Vous pouvez tout de même les essayer et faire des retours sur d'eventuels bug.
+
+
+
+
+Installation : (procédure a suivre en attendant la création d'un installer)
+
+
+Copier / coller les script sur votre system
+
+Ajouter la possibilité de les executer :
+    sudo chmod +x nom_du_script.sh
+
+déplacer et renommer le(s) script(s) dans votre dossier /usr/bin/
+    sudo mv nom_du_script.sh /usr/bin/nom_du_script
+
+vous pouvez maintenant les executer en tappant le nom du script dans votre terminal, comme pour toute autre application
+    sudo nom_du_script
+
+
+
+
+
+Utilisation :
+
+    lamp.sh :
+        
+        Installation d'un serveur web lamp (Linux, Apache2, Mariadb, php)
+
+        provisoirement, il est necessaire d'ajouter un variable d'environnement pour le mot de passe de la base de donnée mariadb.
+
+            export DB_PASSWORD=your_password
+
+        Executer ensuite le script et attendre la fin de l'installation:
+
+            sudo lamp
+
+        Un message vous avertis de la fin de l'installation, votre serveur web est maintenant installé et accessible a l'addresse : http://localhost/
+
+
+
+    wordpress.sh :
+
+        Installation de wordpress sur votre serveur Web
+
+        il est possible en l'etat actuelle d'ajouter l'option -l a la commande afin de lancer le script lamp.sh qui automatise l'installation du serveur lamp (voir consigne d'utilisation du script lamp.sh)
+
+        pour plus d'info sur l'utilisation du script executez le script avec l'option -h ou --help
+
+            wordpress -h   OU   wordpress --help
+
+
+
+
---
+++ Installers/klf-in-wp.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+set -e
+
+function helper() {
+    echo "Usage : $0 [ -d | --database DATABASE ] [ -u | username ADMINNAME ] [ -l | --lamp ] [ -h | --help ]"
+    echo "Options :"
+    echo " -d, --database DATABASE : Database name"
+    echo " -u, --username ADMINNAME : Username of the database administrator"
+    echo " -l, --lamp : This will install a lamp server on this host"
+    echo " -h, --help : Display this help message"
+}
+
+function adminPasswordCreation() {
+    echo "Please enter a new password for the database administration account"
+    read -s WP_ADMIN_PASSWORD
+    echo "Confirm password"
+    read -s PASSCONFIRM
+
+    if [[ ! PASSCONFIRM == WP_ADMIN_PASSWORD ]];then
+        echo "Password not matching, exiting installation process"
+        exit 102
+    fi
+}
+
+function initializeVariables() {
+    WP_DB_NAME=''
+    WP_ADMIN=''
+    WP_ADMIN_PASSWORD=''
+    LAMP_INSTALL=false
+    DATE=$(date --iso-8601)
+    TMP_FOLDER=/tmp/wp
+    LOG_FOLDER=/var/log/wp_install
+    LOG_FILE=wp_install.log
+    APACHE_PATH=/var/www/html
+    OPTS=$(getopt -o hld:u: --long help,lamp,database:,username: -n 'wordpress' -- "$@")
+}
+
+function sortArgs() {
+
+    eval set -- "$OPTS"
+
+    while true; do
+        case "$1" in
+            -d | --database)
+                echo $1
+                WP_DB_NAME=$2
+                shift 2
+                ;;
+            -l | --lamp)
+                echo $1
+                LAMP_INSTALL=TRUE
+                shift 2
+                ;;
+            -u | --username)
+                echo $1
+                WP_ADMIN=$2
+                shift 2
+                ;;
+            -h | --help)
+                echo $0
+                helper
+                shift 2
+                ;;
+            --)
+                echo $1
+                shift
+                break
+                ;;
+            *)
+                echo "$1 : Unknown options"
+                exit 101
+                ;;
+        esac
+    done
+}
+
+function createFiles() {
+    if [[ ! -d $LOG_FOLDER ]]; then
+        sudo mkdir -p $LOG_FOLDER
+        if [[ ! -f $LOG_FOLDER/$LOG_FILE ]]; then
+            sudo touch $LOG_FOLDER/$LOG_FILE
+        fi
+    fi
+    
+    if [[ ! -d $TMP_FOLDER ]]; then
+        sudo mkdir $TMP_FOLDER 1>$LOG_FOLDER/$LOG_FILE 2>&1
+    else
+        sudo rm -r $TMP_FOLDER/
+    fi
+}
+
+initializeVariables
+echo $WP_DB_NAME
+echo $WP_ADMIN
+echo 'test'
+
+createFiles
+sortArgs
+echo $WP_DB_NAME
+echo $WP_ADMIN
+
+    if [[ -z $WP_DB_NAME || -z $WP_ADMIN ]]; then
+        echo 'Missing options'
+        helper
+        exit 101
+    fi
+adminPasswordCreation
+
+if [[$LAMP_INSTALL = true ]];then
+    echo 'The lamp server installation will start soon'
+    if [[ ! command -v lamp ]];then
+        echo "The lamp installation program could not be found on your system, make sure it exist in your '/usr/bin' directory or reinstall the program"
+    else
+        lamp
+    fi
+fi
+
+if [[ -e /var/log/wordpressinstall ]]; then
+    sudo rm -rf /var/log/wordpressinstall
+fi
+
+sudo wget -c https://wordpress.org/latest.zip -O $TMP_FOLDER/latest.zip 1>$LOG_FOLDER/$LOG_FILE 2>&1
+
+sudo mysql -su root -p<<EOS
+CREATE DATABASE wp_'$WP_DB_NAME';
+CREATE USER '$WP_ADMIN'@'localhost' IDENTIFIED BY '$WP_ADMIN_PASSWORD';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO $WP_ADMIN@localhost;
+FLUSH PRIVILEGES;
+EOS 1>$LOG_FOLDER/$LOG_FILE 2>&1
+
+sudo rm $APACHE_PATH/index.html 1>$LOG_FOLDER/$LOG_FILE 2>&1
+
+sudo apt update
+
+if [[ ! command -v zip ]];then
+    sudo apt install zip -y
+fi
+
+sudo unzip $TMP_FOLDER/latest.zip -d $APACHE_PATH 1>$LOG_FOLDER/$LOG_FILE 2>&1
+sudo mv $APACHE_PATH/wordpress/* $APACHE_PATH/ 1>$LOG_FOLDER/$LOG_FILE 2>&1
+sudo rm $APACHE_PATH/wordpress/ -Rf 1>$LOG_FOLDER/$LOG_FILE 2>&1
+
+sudo chown -R www-data:www-data $APACHE_PATH/ 1>$LOG_FOLDER/$LOG_FILE 2>&1
+
+echo 'The latest version of wordpress has been succesfully installed'
+echo 'You can now access your wordpress web interfaces at :'
+echo 'http://localhost/wp-admin/'
