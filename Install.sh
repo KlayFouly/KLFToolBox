1cf92461b5230d1bbcda3966af80d53ca461e210
KlayFouly <baptiste.floch@gmx.fr>
2025-03-25 11:49:51 +0100

    Add The installer script that handle the process of copying and renaming the scripts to user PATH

---
+++ Install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+function helper() {
+    echo " Usage : $0 [ -f | --fresh ] [ -h | --help ]"
+    echo " Options :"
+    echo "    -p, --path : [default : /usr/bin] Define the PATH directory where the scripts will be installed"
+    echo "    -h, --help : Display this help message"
+    exit 0
+}
+function sortArgs(){
+
+    eval set -- "$OPTS"
+
+    while true;do
+        case "$1" in
+            -h | --help)
+                helper
+                shift
+                break
+                ;;
+            -p | --path)
+                uninstallComponent
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "$1 : Unknown options"
+                exit 101
+                ;;
+        esac
+    done
+}
+
+function variablesInitialize(){
+    LOG_DIR=/var/log/klftb/
+    LOG_FILE=installer.log
+    FILES="Installers/*"
+    INSTALLER_DIRECTORY=Installers/
+    PATH_DIR=/usr/bin/
+    OPTS=$(getopt -o hp --long help,path -n 'install' -- "$@")
+}
+
+
+
+varibalesInitialize "$@"
+sudo chmod -R +x Installers/* 
+for f in $FILES
+do
+    echo "Installing $f..."
+    cp $f $PATH_DIR
+    mv "$PATH_DIR${f}" "$PATH_DIR${f/.sh/}" 
+done
---
+++ Installers/klf-in-pxe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install dhcp httpd tftp-server syslinux
